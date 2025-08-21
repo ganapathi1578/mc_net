c8ceeaa83229272470bcd0805626b369920a57cc
ganapathi1578 <ganapathi1578@gmail.con>
2025-08-22 01:07:05 +0530

    Prepare for Render deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Society.wsgi:application
--- mc_net/settings.py
+++ mc_net/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
+import dj_database_url
+
 
 from pathlib import Path
 
@@ -127,8 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
